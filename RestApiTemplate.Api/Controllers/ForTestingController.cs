9eb578f7352470960d06d2acf578b6ad3e5ad6d8
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-04-08 01:52:18 +0800

    modified controller and add testing controller for development phase

---
+++ RestApiTemplate.Api/Controllers/ForTestingController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestApiTemplate.Api.Services;
+using System.Threading.Tasks;
+
+namespace RestApiTemplate.Api.Controllers
+{
+    [Route("api/development/testing")]
+    [ApiController]
+    public class ForTestingController : ControllerBase
+    {
+        private readonly ITemplateService service;
+
+        public ForTestingController(ITemplateService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("get-all-no-filter")]
+        public async Task<IActionResult> Get() 
+        {
+            return Ok(await service.TestingGetAllAsync());        
+        }
+
+        [HttpDelete("reset-database")]
+        public async Task<IActionResult> ResetDatabase()
+        {
+            var resetDb = await service.ResetAllTemplateDatabaseAsync();
+            if (resetDb == false) { return BadRequest(); }
+            return Ok();
+        }
+    }
+}
--- RestApiTemplate.Api/Controllers/TemplateController.cs
+++ RestApiTemplate.Api/Controllers/TemplateController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RestApiTemplate.Api.DTOs;
 using RestApiTemplate.Api.Models;
 using RestApiTemplate.Api.Services;
 using System.Threading.Tasks;
@@ -36,33 +37,70 @@ namespace RestApiTemplate.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody]TemplateModel model) 
+        public async Task<IActionResult> Create([FromBody]TemplateModelDtoPostRequest dto) 
         { 
-            var createdTemplate = await service.CreateTemplateAsync(model);
+            if(!ModelState.IsValid) 
+            {
+                return BadRequest(ModelState); 
+            }
+            try
+            {
+                var (Id, createdTemplate) = await service.CreateTemplateAsync(dto);
 
-            return CreatedAtAction(nameof(ReadById), new { id = createdTemplate.Id }, createdTemplate);
+                return CreatedAtAction(nameof(ReadById), new { id = Id }, createdTemplate);
+            }
+
+            //Optional Use of Problem Details (RFC 7807):
+            catch (Exception)
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Status = 500,
+                    Title = "Internal Server Error",
+                    Detail = "An unexpected error occurred. Please try again later."
+                };
+                return StatusCode(500, problemDetails);
+            }
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id ,[FromBody] TemplateModel model) 
+        public async Task<IActionResult> Update(int id ,[FromBody] TemplateModelDtoPostRequest dto) 
         {
-            var updatedTemplate = await service.UpdateTemplateAsync(id, model);
-            return Ok(updatedTemplate); 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); 
+            }
+
+            try
+            {
+                var updatedTemplate = await service.UpdateTemplateAsync(id, dto);
+                if (updatedTemplate == null) 
+                {
+                    return NotFound(); 
+                }
+
+                return Ok(updatedTemplate);
+            }
+
+            catch 
+            { 
+                return BadRequest();
+            }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> Delete(int id) 
         {
             var deletedTemplate = await service.DeleteTemplateAsync(id);
-            if (deletedTemplate == false) { return NotFound(); }
+            if (deletedTemplate == false) 
+            {
+                return NotFound(); 
+            }
+
             return NoContent(); 
         }
 
-        [HttpDelete]
-        public IActionResult ResetDatabase() 
-        {
-            return Ok(); 
-        }
+        
 
 
     }
