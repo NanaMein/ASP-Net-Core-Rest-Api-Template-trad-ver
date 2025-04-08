9c96d2c5d04e3ac116e6e6814932927ac6ec94cf
Alfeo The Third <alfeoephraimquintay@gmail.com>
2025-04-08 20:53:50 +0800

    update dto

--- RestApiTemplate.Api/DTOs/TemplateModelDtoPostResponse.cs
+++ RestApiTemplate.Api/DTOs/TemplateModelDtoPostResponse.cs
@@ -2,7 +2,7 @@
 {
     public class TemplateModelDtoPostResponse
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateOnly DateOnlyCreated { get; set; }
         public DateTime DateLastModified { get; set; }
     }
